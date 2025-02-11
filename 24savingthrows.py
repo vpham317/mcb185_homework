5e033ae9dafcc79b7154d41a90015c79c4517611
vpham317 <phamvictor317@gmail.com>
2025-02-11 13:53:05 -0800

    update

---
+++ 24savingthrows.py
@@ -0,0 +1,52 @@
+import random
+
+# without advantage and disadvantage
+# DC = 'DC5' , 'DC10', or 'DC15'
+
+def saving(DC):
+	x = random.randint(1, 20)
+	y = random.randint(1, 20)
+	if DC == 'DC15': 
+		if x >= 15: print(x, 'success')
+		elif x < 15: print(x, 'fail')
+	if DC == 'DC5':
+		if x >= 5: print(x, 'success')
+		elif  x < 15: print(x, 'fail')
+	if DC == 'DC10':
+		if x >= 10: print(x, 'success')
+		elif x < 10: print(x, 'fail')
+	
+
+# with advantage and disadvantage
+# R = 'adv', or 'disadv'
+
+def savingR(DC, R):
+	x = random.randint(1, 20)
+	print(x)
+	y = random.randint(1, 20)
+	print(y)
+	if R == 'adv':
+		if x > y: i = x
+		if y > x: i = y
+		if DC == 'DC15': 
+			if i >= 15: print(i, 'success')
+			elif i < 15: print(i, 'fail')
+		if DC == 'DC5':
+			if i >= 5: print(i, 'success')
+			elif  i < 15: print(i, 'fail')
+		if DC == 'DC10':
+			if i >= 10: print(i, 'success')
+			elif i < 10: print(i, 'fail')
+	if R == 'disadv':
+		if x > y: i = y
+		if y > x: i = x
+		if DC == 'DC15': 
+			if i >= 15: print(i, 'success')
+			elif i < 15: print(i, 'fail')
+		if DC == 'DC5':
+			if i >= 5: print(i, 'success')
+			elif  i < 15: print(i, 'fail')
+		if DC == 'DC10':
+			if i >= 10: print(i, 'success')
+			elif i < 10: print(i, 'fail')
+print(savingR('DC15', 'disadv'))
